0b02a3ef219501cfcbcdc99a9f7425b8e7093a38
Flavien124 <101864773+Flavien124@users.noreply.github.com>
2025-03-04 17:40:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 self.addEventListener('install', (event) => {
     console.log('Service Worker installé');
+    self.skipWaiting();  // Force l’installation immédiate
 });
 
 self.addEventListener('activate', (event) => {
     console.log('Service Worker activé');
+    return self.clients.claim();  // Prend en charge toutes les pages ouvertes
 });
